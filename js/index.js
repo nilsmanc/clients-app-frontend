20f45d1b3a9da1ce59ce522831b0c6460efd1dc0
nilsmanc <etereaiberia@gmail.com>
2023-10-15 08:27:15 +0300

    client item

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 import { createClientsHeader } from './createHeader.js'
+import { createClientsSection } from './createClientsSection.js'
+import { getClients } from './clientsApi.js'
+import { createClientItem } from './createClientItem.js'
 
-const createApp = () => {
+const createApp = async () => {
+  const clients = await getClients()
   const header = createClientsHeader()
-  document.body.append(header)
+  const clientSection = createClientsSection()
+  document.body.append(header, clientSection.main)
+
+  for (const client of clients) {
+    document.querySelector('.clients__tbody').append(createClientItem(client))
+  }
 }
 
 createApp()
