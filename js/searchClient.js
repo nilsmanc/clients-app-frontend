cd335d30e480197d7155d8728c0dc870a932e506
nilsmanc <etereaiberia@gmail.com>
2023-11-05 11:27:07 +0300

    search client

--- js/searchClient.js
+++ js/searchClient.js
@@ -1,3 +1,6 @@
+import { findClient } from './clientsApi.js'
+import { createClientItem } from './createClientItem.js'
+
 export const searchClients = (clients) => {
   const findList = document.querySelector('.find-list')
   const input = document.querySelector('.header__input')
@@ -25,4 +28,47 @@ export const searchClients = (clients) => {
       tbody.append(createClientItem(client))
     }
   }
+
+  input.addEventListener('input', async () => {
+    const value = input.value.trim()
+    const foundItems = document.querySelectorAll('.find-list__link')
+
+    if (value !== '') {
+      rewriteTable(value)
+
+      foundItems.forEach((link) => {
+        if (link.innerText.search(value) == -1) {
+          link.classList.add('hide')
+          link.innerHTML = link.innerText
+        } else {
+          link.classList.remove('hide')
+          findList.classList.remove('hide')
+          const str = link.innerText
+          link.innerHTML = insertMark(
+            str,
+            link.innerText.search(value),
+            value.length
+          )
+        }
+      })
+    } else {
+      foundItems.forEach((link) => {
+        const tbody = document.querySelector('.clients__tbody')
+        tbody.innerHTML = ''
+
+        clients.forEach((client) => tbody.append(createClientItem(client)))
+
+        link.classList.remove('hide')
+        findList.classList.add('hide')
+        link.innerHTML = link.innerText
+      })
+    }
+  })
+
+  const insertMark = (str, pos, len) =>
+    str.slice(0, pos) +
+    '<mark>' +
+    str.slice(pos, pos + len) +
+    '</mark>' +
+    str.slice(pos + len)
 }
